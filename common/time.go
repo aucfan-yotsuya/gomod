9d13e9548fedffac07d7e43671e985e05581510a
yotsuya <yotsuya@aucfan.com>
2022-06-20 18:13:35 +0900

    common: NowJST()

--- common/time.go
+++ common/time.go
@@ -7,3 +7,8 @@ func ParseDuration(s string) time.Duration {
 	d, _ = time.ParseDuration(s)
 	return d
 }
+func NowJST() time.Time {
+	var jst *time.Location
+	jst, _ = time.LoadLocation("Asia/Tokyo")
+	return time.Now().In(jst)
+}
