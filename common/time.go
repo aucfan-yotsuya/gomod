2b0f0d0c3620b40433105002e60f7655b2b35078
yotsuya <yotsuya@aucfan.com>
2022-06-23 21:14:38 +0900

    common: NextMonthFirstDay() 追加

--- common/time.go
+++ common/time.go
@@ -8,5 +8,13 @@ func ParseDuration(s string) time.Duration {
 	return d
 }
 func NowJST() time.Time {
-	return time.Now().In(time.FixedZone("JST", 9*60*60))
+	return time.Now().In(time.FixedZone("JST", 9*3600))
+}
+func NextMonthFirstDay() time.Time {
+	var n = NowJST()
+	return time.Date(
+		n.Year(), n.Month(), 1,
+		0, 0, 0, 0,
+		time.FixedZone("JST", 9*3600),
+	).AddDate(0, 1, 0)
 }
